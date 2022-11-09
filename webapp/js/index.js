bd988a7df732a613c930f0e580751a5e2f91c2b9
unknown <ethan.hale@LAUNCHPAD-SPARX.LOCAL>
2022-11-09 11:06:06 -0500

    edit index.js form

--- webapp/js/index.js
+++ webapp/js/index.js
@@ -1,13 +1,18 @@
-const btn = document.getElementById('btn1');
+//const btn = document.getElementById('btn1');
 
-btn.addEventListener('click', () => {
-    const form = document.getElementById('form1');
+//btn.addEventListener('click', () => {
+    //const form = document.getElementById('form1');
 
-    if(form.style.display == 'none') {
+    //if(form.style.display == 'none') {
         //this shows the form
-        form.style.display = 'none';
-    } else {
+        //form.style.display = 'block';
+    //} else {
         //this hides the form
-        form.style.display = 'block';
-    }
+        //form.style.display = 'none';
+    //}
+
+$(document).ready(function(){
+    $('btn1').click(function(){
+        $('form1').toggle(500);
+    });
 });
