8294ce9a6a06a1439dfacba79daf847e6e42f619
unknown <ethan.hale@LAUNCHPAD-SPARX.LOCAL>
2022-11-09 11:55:53 -0500

    update js

--- webapp/js/index.js
+++ webapp/js/index.js
@@ -2,16 +2,13 @@ const btn = document.getElementById('btn1');
 
 window.onload= function(){
     btn.addEventListener('click', () => {
-        const form = document.getElementById('form1').style.display="none";
-    });
+        document.getElementById('form1').style.display="none";
+    })
 }
-    function form1(a) {
-        if (a ==1) {
+    function form1() {
             document.getElementById("form1").style.display="block";
-        } else {
-            document.getElementById("form1").style.display="none";
         }
-    }
+
 
 //window.onload= function(){
    // $(document).ready(function(){
