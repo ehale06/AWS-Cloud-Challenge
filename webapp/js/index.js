91d765de96812b7baea1de22acf9631ae25d411e
unknown <ethan.hale@LAUNCHPAD-SPARX.LOCAL>
2022-11-09 10:58:47 -0500

    update 30

--- webapp/js/index.js
+++ webapp/js/index.js
@@ -3,7 +3,7 @@ const btn = document.getElementById('btn1');
 btn.addEventListener('click', () => {
     const form = document.getElementById('form1');
 
-    if(form.style.display == 'block') {
+    if(form.style.display == 'none') {
         //this shows the form
         form.style.display = 'none';
     } else {
