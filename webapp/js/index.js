078c45cf1b809a1b11f5cdca162fb196fefafa7a
unknown <ethan.hale@LAUNCHPAD-SPARX.LOCAL>
2022-11-09 11:49:07 -0500

    update js 11:49

--- webapp/js/index.js
+++ webapp/js/index.js
@@ -1,9 +1,10 @@
 const btn = document.getElementById('btn1');
 
+window.onload= function(){
     btn.addEventListener('click', () => {
         const form = document.getElementById('form1').style.display="none";
     });
-
+}
     function form1(a) {
         if (a ==1) {
             document.getElementById("form1").style.display="block";
