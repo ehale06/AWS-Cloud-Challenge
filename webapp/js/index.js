b4633ce5f86b70a127c0179a414727dfffd58f07
unknown <ethan.hale@LAUNCHPAD-SPARX.LOCAL>
2022-11-09 10:53:45 -0500

    update hiding form js

--- webapp/js/index.js
+++ webapp/js/index.js
@@ -3,11 +3,11 @@ const btn = document.getElementById('btn1');
 btn.addEventListener('click', () => {
     const form = document.getElementById('form1');
 
-    if(form.style.display === 'none') {
+    if(form.style.display == 'block') {
         //this shows the form
-        form.style.display = 'block';
+        form.style.display = 'none';
     } else {
         //this hides the form
-        form.style.display = 'none';
+        form.style.display = 'block';
     }
 });
